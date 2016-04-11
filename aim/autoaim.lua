9105e4e309d2464a18e41c93039dc306970f8a6c
j000 <jarymut+github@gmail.com>
2016-04-11 22:38:48 +0200

    Better show status as hint, not in chat

--- aim/autoaim.lua
+++ aim/autoaim.lua
@@ -22,5 +22,5 @@ if not _fps_aim_assist and _fps_aim_assist_autoshoot then
 end
 --managers.hud:show_hint({text = 'Auto aim ('.. tostring(_fps_aim_assist) ..')'})
 --managers.chat:_receive_message(1, 'AUTOAIM', _fps_aim_assist and 'ON' or 'OFF', tweak_data.system_chat_color)
-managers.chat:_receive_message(1, 'Sharpshooter', _fps_aim_assist and (_fps_aim_assist_autoshoot and 'AUTOSHOOT' or 'AUTOAIM') or 'OFF', tweak_data.system_chat_color)
-
+--managers.chat:_receive_message(1, 'Sharpshooter', _fps_aim_assist and (_fps_aim_assist_autoshoot and 'AUTOSHOOT' or 'AUTOAIM') or 'OFF', tweak_data.system_chat_color)
+managers.hud:show_hint({text = 'Sharpshooter: ' .. (_fps_aim_assist and (_fps_aim_assist_autoshoot and 'AUTOSHOOT' or 'AUTOAIM') or 'OFF')})
--- aim/autoshoot.lua
+++ aim/autoshoot.lua
@@ -18,5 +18,5 @@ end
 _fps_aim_assist_autoshoot = not _fps_aim_assist_autoshoot
 --managers.hud:show_hint({text = 'Auto shoot ('.. tostring(_fps_aim_assist_autoshoot) ..')'})
 --managers.chat:_receive_message(1, 'AUTOSHOOT', _fps_aim_assist_autoshoot and 'ON' or 'OFF', tweak_data.system_chat_color)
-managers.chat:_receive_message(1, 'Sharpshooter', _fps_aim_assist and (_fps_aim_assist_autoshoot and 'AUTOSHOOT' or 'AUTOAIM') or 'OFF', tweak_data.system_chat_color)
-
+--managers.chat:_receive_message(1, 'Sharpshooter', _fps_aim_assist and (_fps_aim_assist_autoshoot and 'AUTOSHOOT' or 'AUTOAIM') or 'OFF', tweak_data.system_chat_color)
+managers.hud:show_hint({text = 'Sharpshooter: ' .. (_fps_aim_assist and (_fps_aim_assist_autoshoot and 'AUTOSHOOT' or 'AUTOAIM') or 'OFF')})
