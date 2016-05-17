b99c93146934d28c13bfe6e2374462988bbe12de
j000 <jarymut+github@gmail.com>
2016-05-17 23:15:23 +0200

    Renamed autocounter.lua -> autocounter_playerdamage.lua

--- aim/autocounter_playerdamage.lua
+++ aim/autocounter_playerdamage.lua
@@ -1,6 +1,6 @@
 local _damage_bullet = PlayerDamage.damage_bullet
 function PlayerDamage:damage_bullet(attack_data)
-	if attack_data.attacker_unit then
+	if _fps_aim_assist_autocounter and attack_data.attacker_unit then
 		managers.player:player_unit():inventory():equipped_unit():base():check_autoaim2_no_angle_check_enemies_insert(attack_data.attacker_unit:key())
 	end
 	return _damage_bullet(self, attack_data)
